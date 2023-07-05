844e2630377b4e4c774455134f46e22e648db326
unknown <freekodelka86@gmail.com>
2023-07-05 18:27:57 +0300

    Решена задача 52

--- Lesson6/Task32.py
+++ Lesson6/Task32.py
@@ -5,13 +5,16 @@ import random
 
 def inputArray():
     n = int(input('Введите количество элементов массива: '))
-    list_1 = [(random.randint(1, 100)) for i in range(n)]
+    list_1 = [(random.randint(-100, 100)) for i in range(n)]
     return list_1
 
-
-def input_interval(min, max):
-
-    return
+def find_indexes(lst, start_interval, finish_interval):
+    return [i for i in range(len(lst)) if start_interval <= lst[i] <= finish_interval]
 
 list_1 = inputArray()
+n = int(input('Введите начало интервала: '))
+m = int(input('Введите конец интервала: '))
 print(list_1)
+print(f'Индексы элементов вышеуказанного списка, находящиеся в интервале от {n} до {m}: {find_indexes(list_1, n, m)}')
+
+
